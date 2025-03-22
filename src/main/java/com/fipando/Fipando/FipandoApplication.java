c51afe29bc86466db36dc01f517751e7b5280cf7
João Nardi <joaovitornard@gmail.com>
2025-03-22 18:11:09 -0400

    Initial commit

---
+++ src/main/java/com/fipando/Fipando/FipandoApplication.java
@@ -0,0 +1,18 @@
+package com.fipando.Fipando;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipandoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipandoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
