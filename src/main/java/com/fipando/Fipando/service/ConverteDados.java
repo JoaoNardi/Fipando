a07d1eac57108de05c22097167b372684b596fdd
João Nardi <joaovitornard@gmail.com>
2025-03-25 19:01:21 -0400

    Correcao da convercao de dados

--- src/main/java/com/fipando/Fipando/service/ConverteDados.java
+++ src/main/java/com/fipando/Fipando/service/ConverteDados.java
@@ -3,27 +3,34 @@ package com.fipando.Fipando.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fipando.Fipando.model.DadosMarcas;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ConverteDados {
+public class ConverteDados implements IConverteDados{
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+
+    @Override
     public <T> T obterDados(String json, Class<T> classe) {
         try {
-            return objectMapper.readValue(json, classe);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao desserializar JSON para " + classe.getSimpleName(), e);
+            return objectMapper.readValue(json,classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public <T> List<T> obterListaDeDados(String json, TypeReference<List<T>> typeReference) {
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
         try {
-            return objectMapper.readValue(json, typeReference);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao desserializar JSON para lista", e);
+            CollectionType lista = objectMapper.getTypeFactory()
+                    .constructCollectionType(List.class,classe);
+            return objectMapper.readValue(json,lista);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
